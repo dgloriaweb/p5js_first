8612447c768bb3273d0e5c9d513830165301004f
unknown <dgloria.web@gmail.com>
2021-09-03 10:21:48 +0100

    changes

--- scooter.js
+++ scooter.js
@@ -2,6 +2,7 @@ function Scooter(x, y, img) {
   this.pos = createVector(x, y)
   this.vel = createVector()
   this.grav = 0.1
+  this.v = createVector()
 
   this.img = img
 
@@ -18,4 +19,42 @@ function Scooter(x, y, img) {
     this.pos.x = startPos
     this.pos.x -= 20
   }
+  
+this.moveScooter= function() {
+  if (isLeftPressed) {
+    //if reached edge, stop
+    if (scooterPosition.x < 100) {
+      this.isLeftPressed = false
+    }
+    velocity.add(acceleration)
+    scooterPosition.sub(velocity)
+  }
+  if (isRightPressed) {
+    if (scooterPosition.x > width - 100) {
+      this.isRightPressed = false
+    }
+    velocity.add(acceleration)
+    scooterPosition.add(velocity)
+  }
+}
+
+this.updateJumper = function() {
+  v.y += gravity
+  scooterPosition.y += v.y
+  scooterPosition.y = constrain(scooterPosition.y, 0, height - 50)
+}
+this.jump = 
+function jump() {
+  console.log('jump');
+  v.y = -19;
+  //if meets floor, stop. floor is at height - 50
+
+  if (scooterPosition.y >= height - 40) {
+    v.y = -v.y;
+  }
+  if (scooterPosition.y <= 50) {
+    v.y = -v.y;
+  }
+}
+
 }
--- sketch.js
+++ sketch.js
@@ -1,104 +1,64 @@
-var cursorX = 50
-var isLeftPressed = false
-var isRightPressed = false
-var mass = 2
-var position
-var velocity
-var acceleration
-var v
-var gravity = 0.8
-var counter
+var cursorX = 50;
+var isLeftPressed = false;
+var isRightPressed = false;
+var scooterPosition;
+var velocity;
+var acceleration;
+var v;
+var gravity = 0.8;
+var counter;
 
 function preload() {
-  img = loadImage('scooter.png')
+  img = loadImage('scooter.png');
 }
 function setup() {
-  createCanvas(600, 400)
-  position = createVector(width/2, height-100)
-  velocity = createVector(0.5)
-  acceleration = createVector(0.1, 0)
-  scooter = new Scooter(position.x, position.y, img)
+  createCanvas(600, 400);
+  scooterPosition = createVector(width / 2, height - 100);
+  velocity = createVector(0.5);
+  acceleration = createVector(0.1, 0);
+  scooter = new Scooter(scooterPosition.x, scooterPosition.y, img);
 
   //jumper
-  pos = createVector(width >> 1, 50)
-  vel = createVector()
-  v = createVector()
-  counter = 3
+  pos = createVector(width >> 1, 50);
+  v = createVector();
+  counter = 3;
 }
 
 function draw() {
-  background(220)
-  updateJumper()
-  update()
-  imageMode(CENTER)
-  image(this.img, position.x, position.y, 100, 100)
-  ellipse(50, height - 25, 20, 20)
-  ellipse(width - 50, height - 25, 20, 20)
+  background(220);
+  scooter.updateJumper();
+  scooter.moveScooter();
+  imageMode(CENTER);
+  image(this.img, scooterPosition.x, scooterPosition.y, 100, 100);
+  ellipse(50, height - 25, 20, 20);
+  ellipse(width - 50, height - 25, 20, 20);
 }
 function touchEnded() {
-  isLeftPressed = false
-  isRightPressed = false
-  velocity = createVector(0.5)
-  acceleration = createVector(0.3, 0)
-  console.log('ended')
+  isLeftPressed = false;
+  isRightPressed = false;
+  velocity = createVector(0.5);
+  acceleration = createVector(0.3, 0);
+  console.log('ended');
 }
 function touchStarted(event) {
-  touchX = getTouchEventX(event)
-  touchY = getTouchEventY(event)
+  touchX = getTouchEventX(event);
+  touchY = getTouchEventY(event);
   if (touchY < height - 100) {
-    
     //jump
-    jump()
+    scooter.jump();
   } else if (touchX > 25 && touchX < 75) {
-    isLeftPressed = true
-    console.log('left')
+    img = loadImage('scooter_left.png');
+    isLeftPressed = true;
+    console.log('left');
   } else if (touchX > width - 75 && touchX < width - 25) {
-    isRightPressed = true
-  }
-}
-
-function updateJumper() {
-  v.y += gravity
-  position.y += v.y
-  position.y = constrain(position.y, 0, height - 50)
-}
-function jump() {
-  console.log('jump')
-  v.y = -19
-  //if meets floor, stop. floor is at height - 50
-
-  if (position.y >= height - 40) {
-    v.y = -v.y
-  }
-  if (position.y <= 50) {
-    v.y = -v.y
+    img = loadImage('scooter.png');
+    isRightPressed = true;
   }
 }
 
 function getTouchEventX(event) {
-  return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
+  return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
 }
 function getTouchEventY(event) {
-  return event.type.includes('mouse') ? event.pageY : event.touches[0].clientY
-}
-// function createGem() {
-//   gems.push(new Gem(gemX, 150, 20))
-// }
-
-function update() {
-  if (isLeftPressed) {
-    //if reached edge, stop
-    if (position.x < 100) {
-      this.isLeftPressed = false
-    }
-    velocity.add(acceleration)
-    position.sub(velocity)
-  }
-  if (isRightPressed) {
-    if (position.x > width - 100) {
-      this.isRightPressed = false
-    }
-    velocity.add(acceleration)
-    position.add(velocity)
-  }
+  return event.type.includes('mouse') ? event.pageY : event.touches[0].clientY;
 }
